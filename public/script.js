9f2eb52c82e0a424efda367589e30f74632f91e2
Hudayfa Koujdal <102149280+hdif004@users.noreply.github.com>
2023-11-08 11:51:51 +0100

    update du script, j'ai rajouté le fait que le nom du pays dont le drapeau est affiché soit dans la console

--- public/script.js
+++ public/script.js
@@ -1,27 +1,27 @@
 
 
-
-
-
-init();
-
-
-function getRandomFlag(data) {
-    const randomCountryIndex = Math.floor(Math.random() * data.length);
-    const randomCountry = data[randomCountryIndex];
-    const randomFlag = randomCountry.flags.png;
-    return randomFlag;
-}
-
-        function init() {
-            fetch('https://restcountries.com/v3.1/all?fields=flags')
+document.addEventListener('DOMContentLoaded', function () {
+    function init() {
+        fetch('https://restcountries.com/v3.1/all')
             .then(response => response.json())
-                .then(data => {
-                    const randomFlag = getRandomFlag(data);
-                    const drapeau = document.getElementById('drapeau');
-                    drapeau.setAttribute('src', randomFlag);
-                })
-                .catch(error => {
-                    console.error('Une erreur s\'est produite :', error);
-                });
-        };
+            .then(data => {
+                const randomData = getRandomCountry(data); 
+                const randomCountry = randomData.country;
+                const randomFlag = randomData.flag;
+                const drapeau = document.getElementById('drapeau');
+                drapeau.setAttribute('src', randomFlag);
+                console.log(randomCountry.translations.fra.common);
+            })
+            .catch(error => {
+                console.error('Une erreur s\'est produite :', error);
+            });
+    }
+    
+    function getRandomCountry(data) {
+        const randomCountryIndex = Math.floor(Math.random() * data.length);
+        const randomCountry = data[randomCountryIndex];
+        const randomFlag = randomCountry.flags.png;
+        return { country: randomCountry, flag: randomFlag };
+    }
+    init();
+});
