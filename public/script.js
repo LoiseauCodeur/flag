6486238462e097afcaf84c958421b05887a3e984
Robert <119432305+Boushow@users.noreply.github.com>
2023-11-08 23:30:44 -0300

    Test d'un menu externe

--- public/script.js
+++ public/script.js
@@ -1,64 +1,53 @@
 /**************
-*	On ajoute un évènement "lorsque l'arborescence DOM est chargée". 
-*   Ainsi, nous sommes certains de manipuler des élements 
-*	chargés et existants dans notre DOM.
-**************/
+ *	On ajoute un évènement "lorsque l'arborescence DOM est chargée".
+ *   Ainsi, nous sommes certains de manipuler des élements
+ *	chargés et existants dans notre DOM.
+ **************/
 document.addEventListener("DOMContentLoaded", function () {
+  // La fonction init() est apppelée lorsque le DOM est prêt
+  function init() {
+    // fetch() est ici mis pour faire une requête HTTP à l'API des pays
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json()) // La réponse est convertie en JSON
+      .then((data) => {
+        const randomData = getRandomCountry(data); // Un pays choisit aléatoirement est récupérer
+        const randomFlag = randomData.flag; // Le drapeau du pays choisit aléatoirement est récupérer
+        const countryName = randomData.name; // Le nom du pays choisit aléatoirement est récupérer
+        const drapeau = document.getElementById("drapeau");
+        // On fait appele ici à l'élement ayant comme id "drapeau"
+        drapeau.setAttribute("src", randomFlag);
+        // L'attribut "src" de l'élément "drapeau" est difini sur l'URL du drapeau
+        quizz(countryName);
+        // La fonction quizz() est appellée avec le nom du pays en argument
+      })
+      .catch((error) => {
+        console.error("Une erreur s'est produite :", error); // Les erreurs ici s'afficheront dans la console
+      });
 
-  const start = document.getElementById("startButton");
-  const game = document.getElementById("game");
-  const consigne = document.getElementById("règles");
-
-  start.addEventListener('click',function(){
-    start.style.display ='none'
-    consigne.style.display = 'none'
-    game.style.display = 'block';
-    game.style.paddingTop = '100px';
-
-        // La fonction init() est apppelée lorsque le DOM est prêt
-    function init(){
-      // fetch() est ici mis pour faire une requête HTTP à l'API des pays
-      fetch("https://restcountries.com/v3.1/all")
-        .then((response) => response.json()) // La réponse est convertie en JSON
-        .then((data) => {
-          const randomData = getRandomCountry(data); // Un pays choisit aléatoirement est récupérer
-          const randomFlag = randomData.flag; // Le drapeau du pays choisit aléatoirement est récupérer
-          const countryName = randomData.name; // Le nom du pays choisit aléatoirement est récupérer
-          const drapeau = document.getElementById("drapeau");
-          // On fait appele ici à l'élement ayant comme id "drapeau"
-          drapeau.setAttribute("src", randomFlag);
-          // L'attribut "src" de l'élément "drapeau" est difini sur l'URL du drapeau
-          quizz(countryName);
-          // La fonction quizz() est appellée avec le nom du pays en argument 
-        })
-        .catch((error) => {
-          console.error("Une erreur s'est produite :", error); // Les erreurs ici s'afficheront dans la console
-        });
-    
     // La constante quizz() est définie ici pour gérer la fonction devinettes du jeu
     const quizz = (bonneReponse) => {
-      const input = document.querySelector("#okInput");// On fait appele ici à l'élement ayant comme id "okInput"
-      const okButton = document.querySelector("#okButton");// On fait appele ici à l'élement ayant comme id "okButton"
-        
-        // L'événement est lier à l'input. Lorsque la touche "Enter" est appuiyer,
-        // la fonction de vérification est appelée avec la valeur de l'input comme argument.
-        okButton.addEventListener("click", () => {
+      const input = document.querySelector("#okInput"); // On fait appele ici à l'élement ayant comme id "okInput"
+      const okButton = document.querySelector("#okButton"); // On fait appele ici à l'élement ayant comme id "okButton"
+
+      // L'événement est lier à l'input. Lorsque la touche "Enter" est appuiyer,
+      // la fonction de vérification est appelée avec la valeur de l'input comme argument.
+      okButton.addEventListener("click", () => {
+        verification(capitalize(input.value));
+        console.log("byutoon");
+      });
+
+      input.addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
           verification(capitalize(input.value));
-          console.log("byutoon");
-        });
+        }
+      });
 
-        input.addEventListener("keydown", (event) => {
-          if (event.key === "Enter") {
-            verification(capitalize(input.value));
-          }
-        });
-
-    console.log(bonneReponse);
-    // La fonction ici est mise en place afin de vérifier si la réponse de l'utilisateur est correcte
+      console.log(bonneReponse);
+      // La fonction ici est mise en place afin de vérifier si la réponse de l'utilisateur est correcte
       const verification = (reponse) => {
         var score = 0;
-        if (reponse == bonneReponse){
-          score++
+        if (reponse == bonneReponse) {
+          score++;
           console.log(score);
           alert("gagné");
         } else {
@@ -67,11 +56,11 @@ document.addEventListener("DOMContentLoaded", function () {
           alert("NUL !");
           return score;
         }
-      };  
+      };
     };
 
     // function capitalize() est définie pour mettre en majuscule la première lettre d'une chaîne de caractères
-    let capitalize = a => (a && a[0].toUpperCase() + a.slice(1)) || "" //https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+    let capitalize = (a) => (a && a[0].toUpperCase() + a.slice(1)) || ""; //https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 
     // Function permettant de sélectionner un pays aléatoirement à partir des données
     function getRandomCountry(data) {
@@ -81,11 +70,9 @@ document.addEventListener("DOMContentLoaded", function () {
       // Un pays est sélectionné à partir des données
       const randomFlag = randomCountry.flags.png;
       const nameCountry = randomCountry.translations.fra.common;
-      return { country: randomCountry, flag: randomFlag, name : nameCountry };
-      };  
-    };
+      return { country: randomCountry, flag: randomFlag, name: nameCountry };
+    }
+  }
 
-    init();// La fonction init est appelée.
-  });
-
+  init(); // La fonction init est appelée.
 });
