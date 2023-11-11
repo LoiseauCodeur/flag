7ade52d3ecde219235cba8e8a2559f9842786e8e
mohamed <aldanmarki77@gmail.com>
2023-11-12 01:59:24 +0300

    finis

--- public/script.js
+++ public/script.js
@@ -8,21 +8,20 @@
   let input;
   function getRandomCountry(data) {
     const randomCountryIndex = Math.floor(Math.random() * data.length);
-    // Un index est générer aléatoirement
     const randomCountry = data[randomCountryIndex];
-    // Un pays est sélectionné à partir des données
     const randomFlag = randomCountry.flags.png;
     const nameCountry = randomCountry.translations.fra.common;
     return { country: randomCountry, flag: randomFlag, name: nameCountry };
   }
-
 
   function init() {
     input = document.querySelector("#okInput");
 
     recommencer.addEventListener('click', function () {
       recommencer.classList.add('hidden');
-      faute = 0;
+      rate.classList.add('hidden'); 
+      rateReponse.classList.remove('hidden'); 
+      location.reload()
     });
 
     fetch("https://restcountries.com/v3.1/independent?status=true")
@@ -50,8 +49,9 @@
     const okButton = document.querySelector("#okButton");
   
     const verification = (reponse) => {
+      
       console.log(reponse, bonneReponse)
-      if (reponse.trim() === bonneReponse.trim()) {
+      if (transform(reponse)  === transform(bonneReponse) ) {
         correct.classList.add("border-green-500", "shadow-xl", "shadow-green-800");
         correct.classList.remove("border-secondary");
         setTimeout(() => {
@@ -59,8 +59,7 @@
           correct.classList.add("border-secondary");
         }, 1000);
         init();          
-      } else {
-        document.getElementById(`flag-${faute}`).style.opacity = 0.5;
+      } else{
         console.log(reponse, bonneReponse)
         console.log(input.value)
         error.classList.add("error", "border-red-800");
@@ -70,13 +69,16 @@
           error.classList.add("border-secondary");
         }, 1000);
         console.log(faute);
+        rate.classList.remove('hidden'); 
+        rateReponse.classList.remove('hidden'); 
+        rateReponse.innerHTML = bonneReponse
+        document.getElementById(`flag-${faute}`).style.opacity = 0.5;
+
         
         if(faute == 3){
           faute = 1
-          recommencer.classList.remove('hidden'); // Afficher le bouton "Recommencer"
-          for (let i = 0; i <= 3; i++) {
-            document.querySelectorAll(`#flag-${i}`).forEach(flag => flag.style.opacity = 1);
-          }
+          recommencer.classList.remove('hidden'); 
+
         };
         faute++
 
@@ -85,25 +87,27 @@
     };
   
     okButton.addEventListener("click", () => {
-      let reponse = input.value;
-      verification(capitalize(reponse));
+      verification((input.value));
     });
   
     input.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
         event.preventDefault();
-        let reponse = input.value;
-        verification(capitalize(reponse));
+        verification((input.value));
       }
     });
   }
   
   
 
-  let capitalize = (a) => (a && a[0].toUpperCase() + a.slice(1)) || ""; //https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+  let transform = (a) => {
+    return a.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/-/g, "").replace(/ /g, "");
+
+} //https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
 
   const quizz = () => {
-    const okButton = document.querySelector("#okButton"); // On fait appele ici à l'élement ayant comme id "okButton"
+    const rate = document.querySelector("#rate"); 
+    const rateReponse = document.querySelector("#rateReponse"); 
 
     const verification = (reponse) => {
       console.log(reponse, bonneReponse)
@@ -127,30 +131,26 @@
         }, 1000);
         faute++
         console.log(faute);
+        rate.classList.remove('hidden'); 
+        rateReponse.classList.remove('hidden'); 
+        rateReponse.innerHTML = bonneReponse
         
         if(faute == 3){
           document.getElementById(`flag-${faute}`).style.opacity = 0.5;
-        
-          faute = 0
-          recommencer.classList.remove('hidden'); // Afficher le bouton "Recommencer"
+          faute = 1
+          recommencer.classList.remove('hidden'); 
+
         };
       };
       
     };
-    // L'événement est lier à l'input. Lorsque la touche "Enter" est appuiyer,
-    // la fonction de vérification est appelée avec la valeur de l'input comme argument.
-
-  
-    
 
     console.log(bonneReponse);
 
-    // La fonction ici est mise en place afin de vérifier si la réponse de l'utilisateur est correcte
 
-    let error = document.getElementById('drapeau');// Variable error pointant vers l'ément ayant comme ID drapeau
+    let error = document.getElementById('drapeau')
     let correct = document.getElementById('drapeau');
     
-    let faute = 0;
 
     let fl1 = document.getElementById("flag-1");
     let fl2 = document.getElementById("flag-2");
